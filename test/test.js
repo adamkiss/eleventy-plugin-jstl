54471b3c7be4e394742fa323b4453eb35c210a2b
Adam Kiss <iam@adamkiss.com>
2023-10-26 04:33:04 +0200

    OH well

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ const pkg = require("../package.json");
 const fs = require("fs");
 
 test("Simple page", async t => {
-	const elev = new Eleventy('./test/sample-1/page.jstl', './test/sample-1/_site', {
+	const elev = new Eleventy('./test/sample-1/', './test/sample-1/_site', {
 		configPath: "./test/sample-1/eleventy.config.js"
 	});
 	const results = await elev.toJSON();
