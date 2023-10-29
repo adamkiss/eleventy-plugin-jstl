fb7f93ffc68ea0e87b93a8dd222c9315fe8bb1af
Adam Kiss <iam@adamkiss.com>
2023-10-29 10:08:04 +0100

    Fix test

--- test/issues-fixes.js
+++ test/issues-fixes.js
@@ -1,23 +1,19 @@
-const {describe, it} = require("node:test");
+const {test} = require("node:test");
 const assert = require("node:assert/strict");
 const Eleventy = require("@11ty/eleventy");
 
-describe("Fixes", async t => {
-	await it('this in functions', async () => {
-		const elev = new Eleventy('./test/issues-fixes/component-as-fn.jstl.js', './test/issues-fixes/_site', {
-			configPath: "./test/issues-fixes/eleventy.config.js"
-		});
-		const results = await elev.toJSON();
-		assert.equal(results.length, 1);
-		assert.equal(results[0]?.content, '<p>yes</p>')
-	})
+const createIssuesEleventy = filename => new Eleventy(
+	`./test/issues-fixes/${filename}`,
+	'./test/issues-fixes/_site',
+	{configPath: "./test/issues-fixes/eleventy.config.js"}
+)
 
-	await it('using Component as Function', async () => {
-		const elev = new Eleventy('./test/issues-fixes/component-as-fn.jstl', './test/issues-fixes/_site', {
-			configPath: "./test/issues-fixes/eleventy.config.js"
-		});
-		const results = await elev.toJSON();
-		assert.equal(results.length, 1);
-		assert.equal(results[0]?.content, '<p>yes</p>')
-	})
+test("Fixes", async t => {
+	const fix1 = await createIssuesEleventy('this-in-functions.jstl.js').toJSON();
+	assert.equal(fix1.length, 1);
+	assert.equal(fix1[0]?.content, '<p>yes</p>')
+
+	const fix2 = await createIssuesEleventy('component-as-fn.jstl').toJSON();
+	assert.equal(fix2.length, 1);
+	assert.equal(fix2[0]?.content, '<p>yes</p>')
 })
